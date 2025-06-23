0d897247b509c3d6c52cd3026b618b6b539fd418
Vaganuki <hugo.warnotte@gmail.com>
2025-06-23 09:47:42 +0200

    Init de l'architecture

---
+++ src/app/components/events/event-creation/event-creation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-creation',
+  imports: [],
+  templateUrl: './event-creation.component.html',
+  styleUrl: './event-creation.component.scss'
+})
+export class EventCreationComponent {
+
+}
---
+++ src/app/components/user/login-in-screen/login-in-screen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-in-screen',
+  imports: [],
+  templateUrl: './login-in-screen.component.html',
+  styleUrl: './login-in-screen.component.scss'
+})
+export class LoginInScreenComponent {
+
+}
