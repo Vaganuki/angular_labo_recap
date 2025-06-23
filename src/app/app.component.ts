de19200b6a567360f0bd11c49388042617d3c6c0
Vaganuki <hugo.warnotte@gmail.com>
2025-06-23 14:54:07 +0200

    fix: emptied

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {DevComponent} from './components/dev/dev.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DevComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
