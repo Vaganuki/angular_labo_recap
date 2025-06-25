bd9dc2fde17268f3575cb6485e5d74ceb1b79c32
Sleepy <hugo.warnotte@gmail.com>
2025-06-25 17:20:29 +0200

    added favicon

--- src/app/components/shared/desktop/desktop.component.ts
+++ src/app/components/shared/desktop/desktop.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {SoundSystemService} from '../../../services/sound-system.service';
 
 @Component({
   selector: 'app-desktop',
@@ -7,6 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './desktop.component.scss'
 })
 export class DesktopComponent {
+
+  private _soundSystem = inject(SoundSystemService);
+
+  volume = 1;
 
   heure = `${new Date().getHours().toString().padStart(2, '0')} : ${new Date().getMinutes().toString().padStart(2, '0')}`;
   timer  = setInterval(() => {});
@@ -21,4 +26,12 @@ export class DesktopComponent {
     clearInterval(this.timer);
   }
 
+  error(){
+    this._soundSystem.playSound('error', this.volume);
+    this.volume++;
+  }
+
+  viouwm(){
+    this._soundSystem.playSound('microsoft', this.volume);
+  }
 }
