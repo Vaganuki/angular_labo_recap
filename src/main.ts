9d765e36e062c579c45fcf8e5e8fb6d68e0304da
Vaganuki <hugo.warnotte@gmail.com>
2025-06-26 11:26:13 +0200

    Correction du c kc

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,6 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {AppComponent} from './app/app.component';
 import {appConfig} from './app/app.config';
 
-bootstrapApplication(AppComponent,
-  {
-    providers: [
-      provideAnimations() // ✅ animations pour CDK
-    ]
-  },
-  );
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
