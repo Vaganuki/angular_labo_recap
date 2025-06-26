21b7497f31234928abe6245b83073bf64fdc5264
Vaganuki <hugo.warnotte@gmail.com>
2025-06-26 09:47:01 +0200

    approut desktop

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,15 @@
 import { Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
-import { MainPageComponent } from './components/dev/main-page/main-page.component';
+// import { MainPageComponent } from './components/dev/main-page/main-page.component';
 import { HomepageComponent } from './components/shared/homepage/homepage.component';
 import { LoginInScreenComponent } from './components/user/login-in-screen/login-in-screen.component';
 import { SignInScreenComponent }  from './components/user/sign-in-screen/sign-in-screen.component';
+import {DesktopComponent} from './components/shared/desktop/desktop.component';
 
 export const routes: Routes = [
   {
     path: 'main-page',
-    component: MainPageComponent,
+    component: DesktopComponent,
     canActivate: [AuthGuard]
   },
   {
